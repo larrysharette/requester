47776d8968ac75267238adc9876e4c070935304c
Larry Sharette <larrysharette@a-celectric.com>
2018-11-11 21:08:02 -0800

    initial commit

---
+++ src/createManager.js
@@ -0,0 +1,9 @@
+module.exports = function createManager () {
+  function test1() {
+    return 'hello'
+  }
+
+  function getState() {
+    return 'hello again'
+  }
+}
---
+++ test/createManager.test.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const createManager = require('../src/createManager')
+
+describe('createManager', () => {
+  it('exposes public api', () => {
+    const manager = createManager();
+    const methods = Object.keys(manager);
+
+    expect(methods.length).toBe(4);
+    expect(methods).toContain('getData')
+    expect(methods).toContain('actionUrl')
+    expect(methods).toContain('actionLocal')
+    expect(methods).toContain('subscribe')
+  })
+
+  it('throws if URL is not valid', () => {
+    const manager = createManager();
+
+    expect(manager.actionUrl(1)).toThrow()
+    expect(manager.actionUrl({})).toThrow()
+    expect(manager.actionUrl([])).toThrow()
+    expect(manager.actionUrl(() => {})).toThrow()
+    
+    expect(manager.actionUrl('www.googlecom')).toThrow()
+    expect(manager.actionUrl('httosj://www.googlecom')).toThrow()
+
+    expect(manager.actionUrl('www.google.com')).not.toThrow()
+  })
+
+  it('is a valid get request', async () => {
+    const manager = createManager();
+    await manager.actionUrl('www.google.com', 'GET')
+    const data = manager.getData()
+
+    expect(data).toEqual({
+      api: {
+        crews: {
+          getCrewBuilder: {
+            crewList: [],
+
+          }
+        }
+      }
+    })
+  })
+})
